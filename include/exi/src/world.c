7c9c17b8da01605a33e5d35b554296fba40ff05d
Joel Kathi <150131627+Joelkathi@users.noreply.github.com>
2026-09-10 15:19:29 +0530

    Add gravity to world and create a dynamic box

--- include/exi/src/world.c
+++ include/exi/src/world.c
@@ -5,3 +5,21 @@ exi_world_create_box(
     exi_vec3(6, 0.5f, 6),
     EXI_STATIC
 );
+world->gravity = exi_vec3(
+    0.0f,
+    -9.81f,
+    0.0f
+);
+ExiEntityID id =
+    exi_world_create_box(
+        world,
+        "Box_A",
+        exi_vec3(0, 5, 0),
+        exi_vec3(0.5f, 0.5f, 0.5f),
+        EXI_DYNAMIC
+    );
+exi_export_obj(
+    &engine.world,
+    "scenes/exi_world.obj",
+    "scenes/exi_world.mtl"
+);
